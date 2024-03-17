66d5d03fb4c0dcce987f75d12037f4dd236d53fa
robsonstein <robsonstein@gmail.com>
2024-03-17 17:01:18 -0300

    Aula 08

---
+++ 08_DB/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,59 @@
+package br.com.erudio.services;
+
+import br.com.erudio.exceptions.ResourceNotFondExpection;
+import br.com.erudio.model.Person;
+import br.com.erudio.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private final Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    @Autowired
+    PersonRepository repository;
+
+    public List<Person> findAll() {
+        logger.info("Finding all people");
+
+        return repository.findAll();
+    }
+
+
+    public Person findById(Long id) {
+        logger.info("Finding one person");
+
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFondExpection("No records found for this ID!"));
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one person");
+
+        return repository.save(person);
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one person");
+
+        var entity = repository.findById(person.getId()).orElseThrow(() -> new ResourceNotFondExpection("No records found for this ID!"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        return repository.save(entity);
+    }
+
+    public void delete(Long id) {
+        logger.info("Deleting one person");
+
+        var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFondExpection("No records found for this ID!"));
+
+        repository.delete(entity);
+    }
+
+}
